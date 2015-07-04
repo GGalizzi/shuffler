8116ca7d12e955202dab0891e680ae663bc6ee44
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2015-07-04 12:02:01 -0300

    Card styling minor.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+
+  def snippet
+    content.split[0..30].join(" ")
+  end
+  
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -8,4 +8,9 @@ class PostTest < ActiveSupport::TestCase
   def test_valid
     assert post.valid?
   end
+
+  def test_snippet
+    post = Post.new(content: "word " * 250)
+    assert_operator post.snippet.split.length, :<, 45O
+  end
 end
