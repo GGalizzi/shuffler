eb7ff8f00ae2b084db67421b7b0d6734d920d6d1
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2015-07-20 11:10:11 -0300

    Change references to boards

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,4 +1,4 @@
-class BoardsController < ApplicationController
+class DecksController < ApplicationController
   def show
     @deck = Deck.find(params[:id])
   end
--- test/helpers/decks_helper_test.rb
+++ test/helpers/decks_helper_test.rb
@@ -1,7 +1,4 @@
 require "test_helper"
 
 class DecksHelperTest < ActionView::TestCase
-  def test_sanity
-    flunk "Need real tests"
-  end
 end
