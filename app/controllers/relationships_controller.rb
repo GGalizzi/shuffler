2081936345538745dd93258a76a716dd3a5c44c0
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2015-10-12 15:46:38 -0300

    Change to remote, keep html owrkiworking.

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,18 @@ class RelationshipsController < ApplicationController
   def create
     other_user = User.find(params[:relationship][:followed_id])
     current_user.follow other_user
-    redirect_to other_user
+
+    respond_to do |format|
+      format.html { redirect_to other_user }
+    end
   end
 
   def destroy
     other_user = Relationship.find(params[:id]).followed
     current_user.unfollow other_user
-    redirect_to other_user
+
+    respond_to do |format|
+      format.html { redirect_to other_user }
+    end
   end
 end
