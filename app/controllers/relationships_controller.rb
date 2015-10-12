10a33d38eea2944159e0459c0016d687041fe5e1
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2015-10-12 19:53:07 -0300

    Adds AJAX to follow/unfollow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,20 +2,22 @@ class RelationshipsController < ApplicationController
   before_action :logged_check
 
   def create
-    other_user = User.find(params[:relationship][:followed_id])
-    current_user.follow other_user
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow @user
 
     respond_to do |format|
-      format.html { redirect_to other_user }
+      format.html { redirect_to @user }
+      format.js
     end
   end
 
   def destroy
-    other_user = Relationship.find(params[:id]).followed
-    current_user.unfollow other_user
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow @user
 
     respond_to do |format|
-      format.html { redirect_to other_user }
+      format.html { redirect_to @user }
+      format.js
     end
   end
 end
