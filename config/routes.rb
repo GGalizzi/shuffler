d7db053e857cfc51041a62689a584c8d654cbe40
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2015-10-16 11:38:59 -0300

    Adds listing by tag.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'tags/show'
+
   root 'cards#index'
   resources :cards
 
@@ -12,6 +14,7 @@ Rails.application.routes.draw do
 
   resources :decks
   resources :relationships, only: [:create, :destroy]
+  resources :tags, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
